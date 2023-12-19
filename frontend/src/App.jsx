523f61449c5a90e0c64205bbbc64c88137a9674a
Subhani Sayyad <65666841+subhani9@users.noreply.github.com>
2023-12-19 12:59:02 +0530

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Order from "../src/pages/order/Order";
+import Shop from "../src/pages/shop/Shop";
+import Nopage from "./pages/nopage/Nopage";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Srivani</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<Nopage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/nopage/Nopage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nopage = () => {
+  return <div>No Page</div>;
+};
+
+export default Nopage;
